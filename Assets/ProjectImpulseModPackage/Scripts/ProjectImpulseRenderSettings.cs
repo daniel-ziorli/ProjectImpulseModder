7ca4b5ddc648b30691b5aca60bff40ba095ff894
Daniel Ziorli <daniel.ziorli@gmail.com>
2022-10-27 22:16:35 -0400

    added map render settings

---
+++ Assets/ProjectImpulseModPackage/Scripts/ProjectImpulseRenderSettings.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class ProjectImpulseRenderSettings : MonoBehaviour {
+    public Material skyboxMaterial;
+
+    public bool fog = false;
+    public Color fogColor;
+    public float fogDensity;
+    public Color ambientLight;
+    public float haloStrength;
+    public float flareStrength;
+}
--- Assets/ProjectImpulseModPackage/Scripts/ProjectImpulseSkybox.cs
+++
@@ -1,7 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class ProjectImpulseSkybox : MonoBehaviour {
-    public Material skyboxMaterial;
-}
